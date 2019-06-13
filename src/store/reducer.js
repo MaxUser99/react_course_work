720a435bf4a54561db84e34c507cff92aa3c6ade
maxim <m.ossas@cleverdec.com>
2019-06-13 17:14:57 +0300

    fetch fix

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,10 @@
 import actionTypes from "./actionTypes";
+import { loadStatus } from "constants/loadStatus";
 
 const reducer = (state, action) => {
   switch (action.type) {
     case actionTypes.ADD_ITEMS:
-      return { ...state, items: [ ...state.items, ...action.items ] };
+      return { ...state, itemsLoadStatus: loadStatus.LOADED, items: [ ...state.items, ...action.items ] };
     default:
       return state;
   }
